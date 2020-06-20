504c5cb1fc8f97cf2addd10710b0df651545a290
Pins Thoo <aipeng.thoo@gmail.com>
2020-06-20 10:09:14 +0800

    mapped dispatch to props

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,7 +1,21 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { bindActionCreators } from 'redux';
+import { addReminder } from '../actions';
 import '../App.css';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      text: ''
+    }
+  }
+
+  addReminder() {
+    this.props.addReminder(this.state.text);
+  }
+
   render() {
     return (
       <div className="App">
@@ -13,11 +27,13 @@ class App extends Component {
             <input
               className="form-control"
               placeholder="I have to..."
+              onChange={event => this.setState({text: event.target.value})}
             />
           </div>
           <button
             type="button"
             className="btn btn-success"
+            onClick={() => this.addReminder()}
           >
           Add Reminder
           </button>
@@ -27,4 +43,8 @@ class App extends Component {
   }
 }
 
-export default App;
+function mapDispatchToProps(dispatch) {
+  return bindActionCreators({addReminder}, dispatch);
+}
+
+export default connect(null, mapDispatchToProps)(App);
