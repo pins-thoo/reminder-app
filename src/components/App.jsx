ebeea3f40435a498961727cfaecfdd9040490d3a
Pins Thoo <aipeng.thoo@gmail.com>
2020-06-20 10:21:07 +0800

    list of reminders added

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,8 +1,6 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
-import { bindActionCreators } from 'redux';
 import { addReminder } from '../actions';
-import '../App.css';
 
 class App extends Component {
   constructor(props) {
@@ -16,13 +14,31 @@ class App extends Component {
     this.props.addReminder(this.state.text);
   }
 
+  renderReminders() {
+    const { reminders } = this.props;
+    return (
+      <ul className="list-group col-sm-4">
+        {
+          reminders.map(reminder => {
+            return (
+              <li key={reminder.id} className="list-group-item">
+                <div>{reminder.text}</div>
+
+              </li>
+            )
+          })
+        }
+      </ul>
+    )
+  }
+
   render() {
     return (
       <div className="App">
         <div className="title">
           Reminder App
         </div>
-        <div className="form-inline">
+        <div className="form-inline reminder-form">
           <div className="form-group">
             <input
               className="form-control"
@@ -38,13 +54,16 @@ class App extends Component {
           Add Reminder
           </button>
         </div>
+        { this.renderReminders() }
       </div>
     )
   }
 }
 
-function mapDispatchToProps(dispatch) {
-  return bindActionCreators({addReminder}, dispatch);
+function mapStateToProps(state) {
+  return {
+    reminders: state
+  }
 }
 
-export default connect(null, mapDispatchToProps)(App);
+export default connect(mapStateToProps, { addReminder })(App);
